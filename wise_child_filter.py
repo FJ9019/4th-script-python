8ff95580d60f70361208f452aeb9e648ce893409
FJ9019 <feenlus24@gmail.com>
2024-12-16 14:14:19 -0500

    First Save

---
+++ wise_child_filter.py
@@ -0,0 +1,15 @@
+#coding: utf-8
+
+"""
+Objectif : Catégorisé les enfants pour voir s'il ont droit a un cadeaux ou non
+Condition de validité :
+      - age max egale à 10 ans
+      - avoir été sage les 12 derniers mois
+Variable :
+      - nom : (str) le nom de l'enfant
+      - age : (int) l'age de l'enfant
+      - sage : (bool) sage ou non
+Sortie :
+      - (str) Félicitations nom_de_l'enfant tu as été sage, le père Noël attend impatiemment ta lettre
+      - (str) Désolé, nom_de_l'en_enfant tu n'as pas été sage cette année, papa Noël pas content de toi
+"""
